b61799c189f2f8bf529ce016f5babab72eccbf04
krishna-0904 <krishnaprabhas601@gmail.com>
2024-12-17 08:39:07 +0530

    Update if-else.py

--- if-else.py
+++ if-else.py
@@ -1,1 +1,11 @@
-nested
+name=input("enter a name:")
+exp=int(input(f"enter {name} experience:"))
+if exp>1:
+    if exp>=1 and exp<=5:
+        print("salary is 25k")
+    elif exp>=6 and exp<=10:
+        print("salary is 50k")
+    else:
+        print("salary is 75k")
+else:
+    print("not allowed")
