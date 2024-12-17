20c72149b0fa7aed7a6e1e462e0a45dff1b146d2
krishna-0904 <krishnaprabhas601@gmail.com>
2024-12-17 09:00:43 +0530

    Create while true loop.py

---
+++ while true loop.py
@@ -0,0 +1,15 @@
+while True:
+    print("1.payment failed")
+    print("2.profile and privacy")
+    print("3.money transfer")
+    print("4.exit")
+    user_input=int(input("enter ur choice:"))
+    if user_input==1:
+        print("welcome to payment failed")
+    elif user_input==2:
+        print("welcome to profile and privacy")
+    elif user_input==3:
+        print("welcome to money transfer")
+    elif user_input==4:
+        print("exit")
+        break
